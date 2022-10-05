aae8afdbfee08cd4687c20466312b3d7dcfed3fc
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-10-05 17:39:15 -0500

    all

---
+++ App.js
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.subcontainer}>
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btntxt}>+</Text>
+        </TouchableOpacity>
+        <View></View>
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btntxt}>-</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0a0ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  subcontainer:{
+    height: 50,
+    width: '100%',
+    paddingHorizontal:10,
+    flexDirection:'row'
+  },
+  btn:{
+    width:100,
+    height:100,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'#a000e0'
+  }
+});
